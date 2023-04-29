941d617d4edc870ad8541683937e6196590c592a
Shrirang Mhalgi <shrirangmhalgi@gmail.com>
2023-04-29 12:10:30 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import pickle
 from flask import Flask, render_template, request, redirect, url_for, flash
 from python_scripts import eda
-from python_scripts import data_view
+# from python_scripts import data_view
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
@@ -67,108 +67,108 @@ def eda_page():
 def about_us_page():
     return render_template("about-us.html")
 
-# create read update delete page
-@app.route("/data-view", methods=["GET", "POST"])
-def data_view_page():
-    data = data_view.data_view()
-    return render_template("data-view.html", data=data)
+# # create read update delete page
+# @app.route("/data-view", methods=["GET", "POST"])
+# def data_view_page():
+#     data = data_view.data_view()
+#     return render_template("data-view.html", data=data)
 
-@app.route('/add_college', methods=["POST", "GET"])
-def add_college():
-    college_details = {"college_name" : None,
-                        "campus_size": None,
-                        "gender":None,
-                        "total_student_enrollments" : None,
-                        "total_faculty_count": None,
-                        "eastablished_year" : None,
-                        "rating" : None,
-                        "university":None,
-                        "courses": None,
-                        "facility_name": None,
-                        "city": None,
-                        "state": None,
-                        "country":None,
-                        "college_type": None,
-                        "average_fees": None}
+# @app.route('/add_college', methods=["POST", "GET"])
+# def add_college():
+#     college_details = {"college_name" : None,
+#                         "campus_size": None,
+#                         "gender":None,
+#                         "total_student_enrollments" : None,
+#                         "total_faculty_count": None,
+#                         "eastablished_year" : None,
+#                         "rating" : None,
+#                         "university":None,
+#                         "courses": None,
+#                         "facility_name": None,
+#                         "city": None,
+#                         "state": None,
+#                         "country":None,
+#                         "college_type": None,
+#                         "average_fees": None}
 
-    if request.method == "POST":
-        college_details = {"college_name" : request.form["college_name"],
-                        "campus_size": request.form["campus_size"],
-                        "gender": request.form["gender"],
-                        "total_student_enrollments" : request.form["total_student_enrollments"],
-                        "total_faculty_count": request.form["total_faculty_count"],
-                        "eastablished_year" : request.form["eastablished_year"],
-                        "rating" : request.form["rating"],
-                        "university": request.form["university"],
-                        "courses": request.form["courses"],
-                        "facility_name": request.form["facility_name"],
-                        "city": request.form["city"],
-                        "state": request.form["state"],
-                        "country": request.form["country"],
-                        "college_type": request.form["college_type"],
-                        "average_fees": request.form["average_fees"]}
+#     if request.method == "POST":
+#         college_details = {"college_name" : request.form["college_name"],
+#                         "campus_size": request.form["campus_size"],
+#                         "gender": request.form["gender"],
+#                         "total_student_enrollments" : request.form["total_student_enrollments"],
+#                         "total_faculty_count": request.form["total_faculty_count"],
+#                         "eastablished_year" : request.form["eastablished_year"],
+#                         "rating" : request.form["rating"],
+#                         "university": request.form["university"],
+#                         "courses": request.form["courses"],
+#                         "facility_name": request.form["facility_name"],
+#                         "city": request.form["city"],
+#                         "state": request.form["state"],
+#                         "country": request.form["country"],
+#                         "college_type": request.form["college_type"],
+#                         "average_fees": request.form["average_fees"]}
         
-        data_view.add_college(college_details)
+#         data_view.add_college(college_details)
 
 
-        flash("College added successfully")
-        return redirect(url_for('data_view_page'))
+#         flash("College added successfully")
+#         return redirect(url_for('data_view_page'))
 
-@app.route("/edit/<id>", methods=["POST", "GET"])
-def get_college_details(id):
-    college_details = data_view.get_college_details(id)
-    print(college_details)
-    return render_template("edit.html", college_details=college_details)
+# @app.route("/edit/<id>", methods=["POST", "GET"])
+# def get_college_details(id):
+#     college_details = data_view.get_college_details(id)
+#     print(college_details)
+#     return render_template("edit.html", college_details=college_details)
 
 
-@app.route("/update/<id>", methods=["POST"])
-def update_contact(id):
-    college_details = {"college_name" : None,
-                        "campus_size": None,
-                        "gender":None,
-                        "total_student_enrollments" : None,
-                        "total_faculty_count": None,
-                        "eastablished_year" : None,
-                        "rating" : None,
-                        "university":None,
-                        "courses": None,
-                        "facility_name": None,
-                        "city": None,
-                        "state": None,
-                        "country":None,
-                        "college_type": None,
-                        "average_fees": None}
+# @app.route("/update/<id>", methods=["POST"])
+# def update_contact(id):
+#     college_details = {"college_name" : None,
+#                         "campus_size": None,
+#                         "gender":None,
+#                         "total_student_enrollments" : None,
+#                         "total_faculty_count": None,
+#                         "eastablished_year" : None,
+#                         "rating" : None,
+#                         "university":None,
+#                         "courses": None,
+#                         "facility_name": None,
+#                         "city": None,
+#                         "state": None,
+#                         "country":None,
+#                         "college_type": None,
+#                         "average_fees": None}
 
-    if request.method == "POST":
-        college_details = {"college_name" : request.form["college_name"],
-                        "campus_size": request.form["campus_size"],
-                        "gender": request.form["gender"],
-                        "total_student_enrollments" : request.form["total_student_enrollments"],
-                        "total_faculty_count": request.form["total_faculty_count"],
-                        "established_year" : request.form["established_year"],
-                        "rating" : request.form["rating"],
-                        "university": request.form["university"],
-                        "courses": request.form["courses"],
-                        "facility_name": request.form["facility_name"],
-                        "city": request.form["city"],
-                        "state": request.form["state"],
-                        "country": request.form["country"],
-                        "college_type": request.form["college_type"],
-                        "average_fees": request.form["average_fees"]}
+#     if request.method == "POST":
+#         college_details = {"college_name" : request.form["college_name"],
+#                         "campus_size": request.form["campus_size"],
+#                         "gender": request.form["gender"],
+#                         "total_student_enrollments" : request.form["total_student_enrollments"],
+#                         "total_faculty_count": request.form["total_faculty_count"],
+#                         "established_year" : request.form["established_year"],
+#                         "rating" : request.form["rating"],
+#                         "university": request.form["university"],
+#                         "courses": request.form["courses"],
+#                         "facility_name": request.form["facility_name"],
+#                         "city": request.form["city"],
+#                         "state": request.form["state"],
+#                         "country": request.form["country"],
+#                         "college_type": request.form["college_type"],
+#                         "average_fees": request.form["average_fees"]}
     
-        data_view.update_college_details(college_details)
+#         data_view.update_college_details(college_details)
 
 
-    flash("College updated successfully")
-    return redirect(url_for('data_view_page'))
+#     flash("College updated successfully")
+#     return redirect(url_for('data_view_page'))
     
-@app.route("/delete/<id>", methods=["POST", "GET"])
-def delete_record(id):
-    print("Records deleted successfully")
-    data_view.delete_college_details(id)
+# @app.route("/delete/<id>", methods=["POST", "GET"])
+# def delete_record(id):
+#     print("Records deleted successfully")
+#     data_view.delete_college_details(id)
 
-    flash("College Deleted Successfully")
-    return redirect(url_for('data_view_page'))
+#     flash("College Deleted Successfully")
+#     return redirect(url_for('data_view_page'))
 
 
 if __name__ == '__main__':
