4ef91eef4d35d0fb2715fb79cd9a811f56804a7f
Shrirang Mhalgi <shrirangmhalgi@gmail.com>
2023-04-30 02:26:18 -0400

    added the code for deployment

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import pickle
 from flask import Flask, render_template, request, redirect, url_for, flash
-from python_scripts import eda
-# from python_scripts import data_view
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
@@ -18,6 +16,10 @@ gender_pie_chart = None
 facilities_count_plot, facilities_count_list = None, None
 top25_universities_college_count_plot, universities_count_list = None, None
 top25_states_college_count_plot, top25_states_college_count_plot_list = None, None
+data = None
+
+with open('pickles/data_view/all_data.pkl','rb') as f:
+    data = pickle.load(f)
 
 with open('pickles/eda/overview.pkl','rb') as f:
     overview = pickle.load(f)
@@ -67,108 +69,123 @@ def eda_page():
 def about_us_page():
     return render_template("about-us.html")
 
-# # create read update delete page
-# @app.route("/data-view", methods=["GET", "POST"])
-# def data_view_page():
-#     data = data_view.data_view()
-#     return render_template("data-view.html", data=data)
+# create read update delete page
+@app.route("/data-view", methods=["GET", "POST"])
+def data_view_page():
+    # data = data_view.data_view()
 
-# @app.route('/add_college', methods=["POST", "GET"])
-# def add_college():
-#     college_details = {"college_name" : None,
-#                         "campus_size": None,
-#                         "gender":None,
-#                         "total_student_enrollments" : None,
-#                         "total_faculty_count": None,
-#                         "eastablished_year" : None,
-#                         "rating" : None,
-#                         "university":None,
-#                         "courses": None,
-#                         "facility_name": None,
-#                         "city": None,
-#                         "state": None,
-#                         "country":None,
-#                         "college_type": None,
-#                         "average_fees": None}
+    return render_template("data-view.html", data=data)
 
-#     if request.method == "POST":
-#         college_details = {"college_name" : request.form["college_name"],
-#                         "campus_size": request.form["campus_size"],
-#                         "gender": request.form["gender"],
-#                         "total_student_enrollments" : request.form["total_student_enrollments"],
-#                         "total_faculty_count": request.form["total_faculty_count"],
-#                         "eastablished_year" : request.form["eastablished_year"],
-#                         "rating" : request.form["rating"],
-#                         "university": request.form["university"],
-#                         "courses": request.form["courses"],
-#                         "facility_name": request.form["facility_name"],
-#                         "city": request.form["city"],
-#                         "state": request.form["state"],
-#                         "country": request.form["country"],
-#                         "college_type": request.form["college_type"],
-#                         "average_fees": request.form["average_fees"]}
+@app.route('/add_college', methods=["POST", "GET"])
+def add_college():
+    college_details = {"college_name" : None,
+                        "campus_size": None,
+                        "gender":None,
+                        "total_student_enrollments" : None,
+                        "total_faculty_count": None,
+                        "eastablished_year" : None,
+                        "rating" : None,
+                        "university":None,
+                        "courses": None,
+                        "facility_name": None,
+                        "city": None,
+                        "state": None,
+                        "country":None,
+                        "college_type": None,
+                        "average_fees": None}
+
+    if request.method == "POST":
+        college_details = {"college_name" : request.form["college_name"],
+                        "campus_size": request.form["campus_size"],
+                        "gender": request.form["gender"],
+                        "total_student_enrollments" : request.form["total_student_enrollments"],
+                        "total_faculty_count": request.form["total_faculty_count"],
+                        "eastablished_year" : request.form["eastablished_year"],
+                        "rating" : request.form["rating"],
+                        "university": request.form["university"],
+                        "courses": request.form["courses"],
+                        "facility_name": request.form["facility_name"],
+                        "city": request.form["city"],
+                        "state": request.form["state"],
+                        "country": request.form["country"],
+                        "college_type": request.form["college_type"],
+                        "average_fees": request.form["average_fees"]}
         
-#         data_view.add_college(college_details)
+        flag = False #data_view.add_college(college_details)
+        if flag:
+            flash("College added successfully")
+        else:
+            flash("Failed to add college. Enter correct values and try again")
+        return redirect(url_for('data_view_page'))
 
+@app.route("/edit/<id>", methods=["POST", "GET"])
+def get_college_details(id):
+    # get id equal to that corresponding id
+    college_details = None
 
-#         flash("College added successfully")
-#         return redirect(url_for('data_view_page'))
-
-# @app.route("/edit/<id>", methods=["POST", "GET"])
-# def get_college_details(id):
-#     college_details = data_view.get_college_details(id)
-#     print(college_details)
-#     return render_template("edit.html", college_details=college_details)
-
-
-# @app.route("/update/<id>", methods=["POST"])
-# def update_contact(id):
-#     college_details = {"college_name" : None,
-#                         "campus_size": None,
-#                         "gender":None,
-#                         "total_student_enrollments" : None,
-#                         "total_faculty_count": None,
-#                         "eastablished_year" : None,
-#                         "rating" : None,
-#                         "university":None,
-#                         "courses": None,
-#                         "facility_name": None,
-#                         "city": None,
-#                         "state": None,
-#                         "country":None,
-#                         "college_type": None,
-#                         "average_fees": None}
-
-#     if request.method == "POST":
-#         college_details = {"college_name" : request.form["college_name"],
-#                         "campus_size": request.form["campus_size"],
-#                         "gender": request.form["gender"],
-#                         "total_student_enrollments" : request.form["total_student_enrollments"],
-#                         "total_faculty_count": request.form["total_faculty_count"],
-#                         "established_year" : request.form["established_year"],
-#                         "rating" : request.form["rating"],
-#                         "university": request.form["university"],
-#                         "courses": request.form["courses"],
-#                         "facility_name": request.form["facility_name"],
-#                         "city": request.form["city"],
-#                         "state": request.form["state"],
-#                         "country": request.form["country"],
-#                         "college_type": request.form["college_type"],
-#                         "average_fees": request.form["average_fees"]}
+    for college in data:
+        if college['id'] == int(id):
+            college_details = college
+            break
     
-#         data_view.update_college_details(college_details)
+    # college_details =  # data_view.get_college_details(id)
+   
+    return render_template("edit.html", college_details=college_details)
 
 
-#     flash("College updated successfully")
-#     return redirect(url_for('data_view_page'))
+@app.route("/update/<id>", methods=["POST"])
+def update_contact(id):
+    college_details = {"college_name" : None,
+                        "campus_size": None,
+                        "gender":None,
+                        "total_student_enrollments" : None,
+                        "total_faculty_count": None,
+                        "eastablished_year" : None,
+                        "rating" : None,
+                        "university":None,
+                        "courses": None,
+                        "facility_name": None,
+                        "city": None,
+                        "state": None,
+                        "country":None,
+                        "college_type": None,
+                        "average_fees": None}
+
+    if request.method == "POST":
+        college_details = {"college_name" : request.form["college_name"],
+                        "campus_size": request.form["campus_size"],
+                        "gender": request.form["gender"],
+                        "total_student_enrollments" : request.form["total_student_enrollments"],
+                        "total_faculty_count": request.form["total_faculty_count"],
+                        "established_year" : request.form["established_year"],
+                        "rating" : request.form["rating"],
+                        "university": request.form["university"],
+                        "courses": request.form["courses"],
+                        "facility_name": request.form["facility_name"],
+                        "city": request.form["city"],
+                        "state": request.form["state"],
+                        "country": request.form["country"],
+                        "college_type": request.form["college_type"],
+                        "average_fees": request.form["average_fees"]}
     
-# @app.route("/delete/<id>", methods=["POST", "GET"])
-# def delete_record(id):
-#     print("Records deleted successfully")
-#     data_view.delete_college_details(id)
+        flag = False # data_view.update_college_details(college_details)
 
-#     flash("College Deleted Successfully")
-#     return redirect(url_for('data_view_page'))
+        if flag:
+            flash("College updated successfully")
+        else:
+            flash("Failed to update College")
+    
+    return redirect(url_for('data_view_page'))
+    
+@app.route("/delete/<id>", methods=["POST", "GET"])
+def delete_record(id):
+    flag = False# data_view.delete_college_details(id)
+
+    if flag:
+        flash("College Deleted Successfully")
+    else:
+        flash("Failed to delete college.. Please try again later")
+    return redirect(url_for('data_view_page'))
 
 
 if __name__ == '__main__':
--- python_scripts/data_view.py
+++ python_scripts/data_view.py
@@ -1,5 +1,4 @@
 from python_scripts.eda import config
-import mysql.connector
 import pandas as pd
 
 def data_view():
@@ -7,7 +6,13 @@ def data_view():
 
     cursor = universities_database.cursor(dictionary=True)
     query = """
-        select * from indian_colleges
+        select * 
+        from indian_colleges join genders on indian_colleges.gender_id = genders.gender_id
+        join indian_universities on indian_colleges.university_id = indian_universities.university_id
+        join indian_cities on indian_colleges.city_id = indian_cities.city_id 
+        join indian_states on indian_colleges.state_id = indian_states.state_id
+        join country on indian_colleges.country_id = country.country_id
+        join college_type on indian_colleges.college_type_id = college_type.college_type_id;
     """
     cursor.execute(query)
     results = cursor.fetchall()
@@ -15,16 +20,35 @@ def data_view():
     return results
 
 def add_college(college_details):
-    # get all the stuff from each tables and insert in the main table
-    universities_database = config()
+    try:
+        universities_database = config()
 
-    cursor = universities_database.cursor(dictionary=True)
-    query = f"""
-        insert into _dummy values ({1})
-    """
-    cursor.execute(query)
-    universities_database.commit()
-    
+        cursor = universities_database.cursor(dictionary=True)
+        query = f"""
+            insert into indian_colleges (campus_size, gender_id, total_student_enrollments, 
+            total_faculty_count, established_year, rating, university_id, courses, 
+            facilities, city_id, state_id, country_id, college_type_id, average_fees, college_name) 
+            values ({college_details["campus_size"]}, 
+            {college_details["gender"]}. 
+            {college_details["total_student_enrollments"]}, 
+            {college_details["total_faculty_count"]},
+            {college_details["eastablished_year"]},
+            {college_details["rating"]},
+            {college_details["university"]},
+            {college_details["courses"]},
+            {college_details["facility_name"]},
+            {college_details["total_faculty_count"]},
+            {college_details["city"]},
+            {college_details["state"]},
+            {college_details["country"]},
+            {college_details["college_type"]},
+            {college_details["average_fees"]}
+            )
+        """
+        cursor.execute(query)
+        universities_database.commit()
+    except:
+       return False
 
     return True
 
@@ -34,35 +58,63 @@ def get_college_details(id):
 
     cursor = universities_database.cursor(dictionary=True)
     query = f"""
-        select * from indian_colleges where id = {id}
+        select * 
+        from indian_colleges join genders on indian_colleges.gender_id = genders.gender_id
+        join indian_universities on indian_colleges.university_id = indian_universities.university_id
+        join indian_cities on indian_colleges.city_id = indian_cities.city_id 
+        join indian_states on indian_colleges.state_id = indian_states.state_id
+        join country on indian_colleges.country_id = country.country_id
+        join college_type on indian_colleges.college_type_id = college_type.college_type_id
+        where indian_colleges.id = {id};
     """
     cursor.execute(query)
     results = cursor.fetchall()
-    
+  
     return results[0]
 
 def update_college_details(college_details):
-     # get all the stuff from each tables and insert in the main table
-    universities_database = config()
+    try:
+        # get all the stuff from each tables and insert in the main table
+        universities_database = config()
 
-    cursor = universities_database.cursor(dictionary=True)
-    query = f"""
-        update _dummy set id = 10 where id = 1
-    """
-    cursor.execute(query)
-    universities_database.commit()
-    print("college updated successfully", college_details)
+        cursor = universities_database.cursor(dictionary=True)
+        query = f"""
+            update indian_colleges set campus_size = {college_details["campus_size"]},
+            gender_id = {college_details["gender"]},
+            total_student_enrollments = {college_details["total_student_enrollments"]}, 
+            total_faculty_count = {college_details["total_faculty_count"]},
+            established_year =  {college_details["eastablished_year"]},
+            rating = {college_details["rating"]},
+            university_id = {college_details["university"]},
+            courses = {college_details["courses"]},
+            facilities =  {college_details["facility_name"]},
+            city_id = {college_details["city"]},
+            state_id = {college_details["state"]},
+            country_id = {college_details["country"]},
+            college_type_id = {college_details["college_type"]},
+            average_fees = {college_details["average_fees"]},
+            college_name = {college_details["college_name"]}        
+            where id = {id}
+        """
+        cursor.execute(query)
+        universities_database.commit()
+    except:
+        return False
+
     return True
 
 def delete_college_details(id):
-    universities_database = config()
+    try:
+        universities_database = config()
 
-    cursor = universities_database.cursor(dictionary=True)
-    query = f"""
-       delete from _dummy where id = 10
-    """
-    cursor.execute(query)
-    universities_database.commit()
-    print("deleted records")
+        cursor = universities_database.cursor(dictionary=True)
+        query = f"""
+        delete from indian_colleges where id = {id}
+        """
+        cursor.execute(query)
+        universities_database.commit()
+        print("deleted records")
+    except:
+        return False
     return True
     
